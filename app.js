3f2fe434dfa162f5f0070e9db31ac60b385defa3
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-27 01:14:28 +0100

    initial after some progress

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const  port = process.env.PORT || 5000
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//static files
+
+app.use(express.static('public'));
+
+
+//Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+//Home 
+
+app.get('/', (req,res) => {
+
+  const locals = {
+    title: 'Nodejs',
+    description: 'Free nodejs management system'
+  }
+  res.render('index',locals);
+})
+
+app.listen(port, ()=>{
+  console.log(`App running on port ${port}`);
+}); 
